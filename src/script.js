5ffad52e014a1dd3e3995dc0ffac2495777114e5
sewmisithara <sewmisithara23520@gmail.com>
2023-07-16 12:26:07 +0530

    run animation end

--- src/script.js
+++ src/script.js
@@ -1,22 +1,51 @@
 var animal = document.getElementById("animal");
-idleImageNumber = 0;
+let idleImageNumber = 0;
+let backgroundImagePositionX = 0;
+var moveBackgroundAnimationId = 0;
+var runImageNumber = 0;
+let runAnimationNumber = 0;
+
 function idleAnimation() {
+  if (idleImageNumber >= 10) {
+    idleImageNumber = 0;
+  }
 
-    if (idleImageNumber >= 10) {
-        idleImageNumber = 0;
+  idleImageNumber = idleImageNumber + 1;
+  console.log(idleImageNumber);
+
+  animal.src = "image/Idle (" + idleImageNumber + ").png";
+}
+const idleAnimationStart = setInterval(idleAnimation, 50);
+
+function runAnimation() {
+  runImageNumber = runImageNumber + 1;
+
+  if (runImageNumber == 8) {
+    runImageNumber = 1;
+  }
+  animal.src = "image/Run (" + runImageNumber + ").png";
+}
+function runAnimationStart() {
+  runAnimationNumber = setInterval(runAnimation, 100);
+  clearInterval(idleAnimationStart);
+}
+
+function keyCheck(event) {
+  //alert(event.which);
+  //enter=13
+  var keyCode = event.which;
+  if (keyCode == 13) {
+    if (runAnimationNumber == 0) {
+      runAnimationStart();
     }
-
-    idleImageNumber = idleImageNumber + 1;
-    console.log(idleImageNumber);
-
-    animal.src = "image/Idle (" + idleImageNumber + ").png";
+  }
+  if (moveBackgroundAnimationId == 0) {
+    moveBackgroundAnimationId = setInterval(moveBackground, 50);
+  }
 }
-function idleAnimationStart() {
-    console.log('start');
-    setInterval(idleAnimation, 50);
-}
-function runAnimation(){
 
-
-    
+function moveBackground() {
+  backgroundImagePositionX = backgroundImagePositionX - 20;
+  document.getElementById("background").style.backgroundPositionX =
+    backgroundImagePositionX + "px";
 }
