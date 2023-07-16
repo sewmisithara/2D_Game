4283dfcd2475ddffc7a63d54a575ea9da6df06e9
sewmisithara <sewmisithara23520@gmail.com>
2023-07-16 16:48:38 +0530

    run complete

--- src/script.js
+++ src/script.js
@@ -1,51 +1,48 @@
 var animal = document.getElementById("animal");
-let idleImageNumber = 0;
-let backgroundImagePositionX = 0;
-var moveBackgroundAnimationId = 0;
-var runImageNumber = 0;
-let runAnimationNumber = 0;
-
+idleImageNumber = 0;
+idleAnimationNumber=0;
 function idleAnimation() {
-  if (idleImageNumber >= 10) {
-    idleImageNumber = 0;
-  }
 
-  idleImageNumber = idleImageNumber + 1;
-  console.log(idleImageNumber);
-
-  animal.src = "image/Idle (" + idleImageNumber + ").png";
-}
-const idleAnimationStart = setInterval(idleAnimation, 50);
-
-function runAnimation() {
-  runImageNumber = runImageNumber + 1;
-
-  if (runImageNumber == 8) {
-    runImageNumber = 1;
-  }
-  animal.src = "image/Run (" + runImageNumber + ").png";
-}
-function runAnimationStart() {
-  runAnimationNumber = setInterval(runAnimation, 100);
-  clearInterval(idleAnimationStart);
-}
-
-function keyCheck(event) {
-  //alert(event.which);
-  //enter=13
-  var keyCode = event.which;
-  if (keyCode == 13) {
-    if (runAnimationNumber == 0) {
-      runAnimationStart();
+    idleImageNumber = idleImageNumber + 1;
+    if (idleImageNumber == 10) {
+        idleImageNumber = 1;
     }
-  }
-  if (moveBackgroundAnimationId == 0) {
-    moveBackgroundAnimationId = setInterval(moveBackground, 50);
-  }
+
+    
+    
+
+    animal.src = "image/Idle (" + idleImageNumber + ").png";
+}
+function idleAnimationStart() {
+    
+   idleAnimationNumber= setInterval(idleAnimation, 200);
+}
+runImageNumber= 0;
+runAnimationNumber= 0;
+function runAnimation(){
+    runImageNumber=runImageNumber + 1;
+    if (runImageNumber == 11) {
+        runImageNumber = 1;
+    }
+
+
+    animal.src = "image/run (" + runImageNumber + ").png"; 
+}
+function runAnimationStart(){
+   runAnimationNumber = setInterval(runAnimation,100);
+   clearInterval(idleAnimationNumber);
 }
 
-function moveBackground() {
-  backgroundImagePositionX = backgroundImagePositionX - 20;
-  document.getElementById("background").style.backgroundPositionX =
-    backgroundImagePositionX + "px";
+function keyCheck(event){
+//alert(event.which);
+//enter=13;
+
+var keyCode=event.which;
+
+if(keyCode == 13){
+if(runAnimationNumber == 0){
+ runAnimationStart();
 }
+ }
+}
+
